9f65e0a18594a9ef8246d316008c6a70382c1bf9
TbcProjects <daniel@thebikeclub.co.uk>
2023-04-15 18:42:12 +0100

    Add colors config to theme

--- src/theme/foundations/colors.ts
+++ src/theme/foundations/colors.ts
@@ -1,3 +1,11 @@
-const colors = {};
+const colors = {
+  seraphim: "#D87D4A",
+  chaos_black: "#101010",
+  beluga: "#F1F1F1",
+  dr_white: "#FAFAFA",
+  spice_pink: "#fbaf85",
+  white: "#FFFFFF",
+  black: "#000000",
+};
 
 export default colors;
