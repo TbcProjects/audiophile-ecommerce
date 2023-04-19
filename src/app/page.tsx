22610d45bf330b17c8778b5d6cf9c3c55a738c3c
TbcProjects <daniel@thebikeclub.co.uk>
2023-04-19 18:24:12 +0100

    Create index/home page - wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,31 @@
 "use client";
+import client from "@/lib/commerce";
+
+import { use } from "react";
 
 import { Container } from "@chakra-ui/react";
 
-export default async function HomePage() {
+const fetchCommerce = async () => {
+  const data = await client.categories.list();
+  return data;
+};
+
+export default function HomePage() {
+  const categories = use(fetchCommerce());
+
+  const categoryList = categories.data.map((category) => {
+    return (
+      <div key={category.id}>
+        <p>{category.name}</p>
+      </div>
+    );
+  });
+
   return (
     <main>
       <Container>
         <h1>Audiophile Storefront</h1>
+        {categoryList}
       </Container>
     </main>
   );
