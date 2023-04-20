b218e83cc0de5b855701fd161b57d622abaeb633
TbcProjects <daniel@thebikeclub.co.uk>
2023-04-20 20:09:37 +0100

    Update dato request with sdk

--- src/lib/clients/datocms.ts
+++ src/lib/clients/datocms.ts
@@ -2,32 +2,6 @@ import { GraphQLClient } from "graphql-request";
 
 import { getSdk } from "@/types/Dato/datoTypes";
 
-interface RequestArgs {
-  query: string;
-  variables?: Record<string, unknown>;
-  includeDrafts?: boolean;
-  excludeInvalid?: boolean;
-}
-
-export function request({
-  query,
-  variables,
-  includeDrafts,
-  excludeInvalid,
-}: RequestArgs) {
-  const headers: Record<string, string> = {
-    authorization: `Bearer ${process.env.NEXT_DATOCMS_API_TOKEN}`,
-  };
-  if (includeDrafts ?? false) {
-    headers["X-Include-Drafts"] = "true";
-  }
-  if (excludeInvalid ?? false) {
-    headers["X-Exclude-Invalid"] = "true";
-  }
-  const client = new GraphQLClient("https://graphql.datocms.com", { headers });
-  return client.request(query, variables);
-}
-
 export const configureSdk = (preview = false) => {
   const headers: Record<string, string> = {
     authorization: `Bearer ${process.env.NEXT_DATOCMS_API_TOKEN}`,
