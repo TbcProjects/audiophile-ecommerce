762c22097a3732ae49d21616f2c1a38a7d24826a
TbcProjects <daniel@thebikeclub.co.uk>
2023-04-20 20:10:15 +0100

    Move client sdk to client folder

--- src/lib/clients/commerce.ts
+++ src/lib/clients/commerce.ts
@@ -1,7 +1,7 @@
 import CommerceSDK from "@chec/commerce.js";
 
 const client = new CommerceSDK(
-  process.env.NEXT_PUBLIC_CHEC_PUBLIC_TEST_API_KEY as string
+  process.env.NEXT_PUBLIC_CHEC_PUBLIC_PROD_API_KEY as string
 );
 
 export default client;
