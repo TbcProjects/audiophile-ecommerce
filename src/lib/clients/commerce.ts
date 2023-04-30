c5e07c91480462e36c0751663a626143db03a5a0
TbcProjects <daniel@thebikeclub.co.uk>
2023-04-30 20:02:24 +0100

    TEST: Try Live commerce.js key

--- src/lib/clients/commerce.ts
+++ src/lib/clients/commerce.ts
@@ -1,7 +1,7 @@
 import CommerceSDK from "@chec/commerce.js";
 
 const client = new CommerceSDK(
-  process.env.NEXT_PUBLIC_CHEC_PUBLIC_TEST_SECRET_KEY as string
+  process.env.NEXT_PUBLIC_CHEC_PUBLIC_LIVE_API_KEY as string
 );
 
 export default client;
