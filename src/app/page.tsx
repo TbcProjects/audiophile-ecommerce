12f5c4d00ddef8a8bc230249b9d4a49ba3878df7
TbcProjects <daniel@thebikeclub.co.uk>
2023-04-30 12:55:59 +0100

    Update index page with HomePage wrapper component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,9 @@
-import { fetchCommerce, fetchContent } from "@/lib/fetchers";
-
-import CategoryList from "@/components/home/CategoryList";
-import Heading from "@/components/shared/Heading";
 import HomePageContainer from "@/components/home/HomePageContainer";
 
 export default async function HomePage() {
-  const categories = await fetchCommerce();
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const content = (await fetchContent()) as any;
-
-  const heading = content.allPages[0].heading[0].heading;
-
   return (
-    <main>
-      <HomePageContainer>
-        <Heading heading={heading} />
-        <CategoryList categories={categories} />
-      </HomePageContainer>
-    </main>
+    <HomePageContainer>
+      <h1>Audiophile</h1>
+    </HomePageContainer>
   );
 }
