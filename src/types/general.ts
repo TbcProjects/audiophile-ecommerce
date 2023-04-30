3d0566a8d40a591bbd7c994d89fd7fb7b3dcea3f
TbcProjects <daniel@thebikeclub.co.uk>
2023-04-30 12:47:28 +0100

    Add CustomIconProps

--- src/types/general.ts
+++ src/types/general.ts
@@ -1,5 +1,12 @@
+import type { SVGProps } from "react";
+import type { IconProps, OmitCommonProps } from "@chakra-ui/react";
+
 export type Params = {
   params: {
     slug: string;
   };
 };
+
+export type CustomIconProps = JSX.IntrinsicAttributes &
+  OmitCommonProps<SVGProps<SVGSVGElement>, keyof IconProps> &
+  IconProps & { as?: "svg" | undefined };
