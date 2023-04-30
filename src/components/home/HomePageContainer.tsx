3ec1abafdb432e6807bf28371c74d0eaecd4a3c9
TbcProjects <daniel@thebikeclub.co.uk>
2023-04-30 12:52:56 +0100

    Add HomePage wrapper component

--- src/components/home/HomePageContainer.tsx
+++ src/components/home/HomePageContainer.tsx
@@ -1,18 +1,26 @@
 "use client";
 
-import { ReactNode } from "react";
+import { useQuery } from "@apollo/client";
+import { HOMEPAGE_QUERY } from "../../lib/gql/queries/dato";
+import type { MyQuery } from "@/lib/gql/queries/__generated__/MyQuery";
+import type { ReactNode } from "react";
+
+import { Container, Box } from "@chakra-ui/react";
 
 interface HomePageContainerProps {
   children: ReactNode;
 }
 
-import { Container, Box } from "@chakra-ui/react";
-
 export default function HomePageContainer({
   children,
 }: HomePageContainerProps) {
+  const { data } = useQuery<MyQuery>(HOMEPAGE_QUERY);
+
+  console.log(data?.pageHome);
+
   return (
-    <Box bg="blue.300" h="100vh">
+    <Box bg="black">
+      <h1>Hello</h1>
       <Container h="100%">{children}</Container>
     </Box>
   );
