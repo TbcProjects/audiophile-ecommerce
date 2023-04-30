becb6c3070f774749ba2ea480919a7e671d95761
TbcProjects <daniel@thebikeclub.co.uk>
2023-04-30 20:18:59 +0100

    TEST: Try key again

--- src/lib/clients/commerce.ts
+++ src/lib/clients/commerce.ts
@@ -1,7 +1,9 @@
 import CommerceSDK from "@chec/commerce.js";
 
 const client = new CommerceSDK(
-  process.env.NEXT_PUBLIC_CHEC_PUBLIC_LIVE_API_KEY as string
+  process.env.NEXT_PUBLIC_CHEC_PUBLIC_LIVE_API_KEY as string,
+  false,
+  { cartLifetime: 1 }
 );
 
 export default client;
